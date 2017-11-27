b4b704e0f018e4881d96ac86c11934661d7a98d9
John Lauer as ChiliPeppr on Github <jlauer@chilipeppr.com>
2017-11-27 02:46:32 +0000

    Edited some stuff

---
+++ joystick_example.lua
@@ -0,0 +1,33 @@
+-- ChiliPeppr Joystick test 
+-- Visit http://chilipeppr.com/esp32 for more details
+
+joystick = require("joystick")
+
+joystick.on("xy", function(x, y)
+  print("Got xy change. x:", x, "y:", y)
+end)
+
+-- joystick.on("x", function(x)
+--   print("Got x change. x:", x)
+-- end)
+
+-- joystick.on("y", function(y)
+--   print("Got y change. y:", y)
+-- end)
+
+joystick.on("xcenter", function()
+  print("Got x center")
+end)
+
+joystick.on("ycenter", function()
+  print("Got y center")
+end)
+
+joystick.on("button", function(val)
+  print("Got button:", val)
+end)
+
+joystick.init()
+
+
+                            
